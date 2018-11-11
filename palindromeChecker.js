b9d582412568930e6f710993985884c53d8b505e
LonnieCoffman <LonnieCoffman@users.noreply.github.com>
2018-11-10 16:54:55 -0800

    Create palindromeChecker.js

---
+++ palindromeChecker.js
@@ -0,0 +1,11 @@
+function convertToRoman(num) {
+  let numeral = '';
+  let number = [ 1,4,5,9,10,40,50,90,100,400,500,900,1000,4000];
+  let roman  = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M','MMMM']
+  while (num > 0){
+    let idx = number.findIndex((el) => el > num) - 1;
+    num -= number[idx];
+    numeral += roman[idx];
+  }
+  return numeral;
+}
