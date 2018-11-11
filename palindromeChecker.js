a65f3551537d9793aa62dc63eaed3dc5670b0a00
LonnieCoffman <LonnieCoffman@users.noreply.github.com>
2018-11-10 17:04:24 -0800

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,11 +1,5 @@
-function convertToRoman(num) {
-  let numeral = '';
-  const number = [ 1,4,5,9,10,40,50,90,100,400,500,900,1000,4000];
-  const roman  = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M','MMMM']
-  while (num > 0){
-    let idx = number.findIndex((el) => el > num) - 1;
-    num -= number[idx];
-    numeral += roman[idx];
-  }
-  return numeral;
+function palindrome(str){
+  str = str.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+  const revStr = str.split('').reverse().join('');
+  return(str === revStr);
 }
